ccd6ec902dae97ff630f9d8e6b80103b10aceb03
kikinbmk10 <kikinbmk10@gmail.com>
2017-12-18 12:57:09 -0600

    cambios finales y probar el sistema de correos

--- nano-gasa.php
+++ nano-gasa.php
@@ -25,10 +25,7 @@ include ("lang_".$_SESSION["idiomas"].".php");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Nano | Nano Gasa</title>
-  <link rel="icon" type="image/png" href="https://dummyimage.com/64x64/000/fff" sizes="64x64">
-  <link rel="icon" type="image/png" href="https://dummyimage.com/180x180/000/fff" sizes="180x180">
-  <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
-  <!-- <link rel="icon" type="image/png" href="assets/images/favicon-180x180.png" sizes="128x128"> -->
+  <link rel="icon" type="image/png" href="assets/images/favicon.png" sizes="64x64">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
   <link href="./assets/styles/main.css" rel="stylesheet">
 </head>
